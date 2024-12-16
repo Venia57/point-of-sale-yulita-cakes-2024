48535170afb3d3dad149952073db579cda18584f
Venia57 <145087268+Venia57@users.noreply.github.com>
2024-12-16 15:27:29 +0700

    Update nota.blade.php

--- resources/views/pages/nota.blade.php
+++ resources/views/pages/nota.blade.php
@@ -61,8 +61,8 @@
     </style>
 </head>
 <body>
-    <h1>Henx's</h1>
-    <h2>ALAMAT TOKO</h2>
+    <h1>Yulita Cakes</h1>
+    <h2>Kab. Kubu Raya, Desa Kapur/h2>
     <p>Tanggal: {{ date('d-m-Y') }} {{ date('H:i:s') }}</p>
     <p>No: {{ str_pad($penjualan->id, 10, '0', STR_PAD_LEFT) }}</p>
     <p>Kasir: {{ $penjualan->nama_kasir }}</p>
