be6a3c06d41cdf49ec2e41ea9978ee8e621ab617
Venia57 <145087268+Venia57@users.noreply.github.com>
2025-01-05 00:12:34 +0700

    Update 2024_07_19_181043_create_produks_table.php

--- database/migrations/2024_07_19_181043_create_produks_table.php
+++ database/migrations/2024_07_19_181043_create_produks_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('produks', function (Blueprint $table) {
             $table->id();
-            $table->string('kode_produk')->unique();
             $table->string('nama_produk')->unique();
             $table->unsignedBigInteger('id_kategori');
             $table->integer('harga_modal');
